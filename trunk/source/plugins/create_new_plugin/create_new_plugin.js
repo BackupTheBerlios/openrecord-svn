c8f0a8096f106b0a8631d98fbd5a2d969d6774c2
skinner <skinner@fa7cf185-f9f2-0310-86ed-83ad1049b242>
2005-12-06 01:19:12 +0000

    Made the "create_new_plugin" folder, with an HTML/JavaScript tool for generating new plugins.

---
+++ trunk/source/plugins/create_new_plugin/create_new_plugin.js
@@ -0,0 +1,97 @@
+/*****************************************************************************
+ create_new_plugin.js
+ 
+******************************************************************************
+ Written in 2005 by 
+   Brian Douglas Skinner <brian.skinner@gumption.org>
+   Mignon Belongie
+
+ Copyright rights relinquished under the Creative Commons  
+ Public Domain Dedication:
+    http://creativecommons.org/licenses/publicdomain/
+  
+ You can copy freely from this file.  This work may be freely reproduced, 
+ distributed, transmitted, used, modified, built upon, or otherwise exploited
+ by anyone for any purpose.
+  
+ This work is provided on an "AS IS" basis, without warranties or conditions 
+ of any kind, either express or implied, including, without limitation, any 
+ warranties or conditions of title, non-infringement, merchantability, or 
+ fitness for a particular purpose. You are solely responsible for determining 
+ the appropriateness of using or distributing the work and assume all risks 
+ associated with use of this work, including but not limited to the risks and 
+ costs of errors, compliance with applicable laws, damage to or loss of data 
+ or equipment, and unavailability or interruption of operations.
+
+ In no event shall the authors or contributors have any liability for any 
+ direct, indirect, incidental, special, exemplary, or consequential damages,
+ however caused and on any theory of liability, whether in contract, strict 
+ liability, or tort (including negligence), arising in any way out of or in 
+ connection with the use or distribution of the work.
+*****************************************************************************/
+ 
+dojo.hostenv.setModulePrefix("orp", "../../../source"); // relative to dojo
+dojo.require("dojo.text.*");
+dojo.require("dojo.event.*");
+dojo.require("orp.uuid.TimeBasedUuid");
+
+function startHere() {
+  var createButton = document.getElementById("button");
+  dojo.event.connect(createButton, "onclick", userClickedOnButton);
+
+  var outputArea = document.getElementById("output_area");
+  outputArea.style.visibility = "hidden";
+}
+
+function userClickedOnButton() {
+  var classNameField = document.getElementById("plugin_class_name");
+  var nameField = document.getElementById("plugin_name");
+  var authorField = document.getElementById("plugin_author");
+  var outputArea = document.getElementById("output_area");
+  
+  var className = classNameField.value;
+  var name      = nameField.value;
+  var author    = authorField.value;
+
+  var generatedContents = generateContents(className, name, author);
+
+  outputArea.value = generatedContents;
+  outputArea.style.visibility = "visible";
+  // alert(generatedContents);
+  
+  // var uuid = new orp.uuid.TimeBasedUuid();
+  // alert(uuid);
+  
+  // var string = "     Just Foo    ";
+  // var trimmedString = dojo.text.trim(string);
+  // alert(trimmedString);
+}
+
+function generateContents(className, name, author) {
+  var templateFileContents = dojo.hostenv.getText("create_new_plugin_template.js");
+  var itemUuid = new orp.uuid.TimeBasedUuid();
+  var entryUuid1 = new orp.uuid.TimeBasedUuid();
+  var entryUuid2 = new orp.uuid.TimeBasedUuid();
+  var entryUuid3 = new orp.uuid.TimeBasedUuid();
+  var entryUuid4 = new orp.uuid.TimeBasedUuid();
+  
+  var intermediateResult = templateFileContents;
+  intermediateResult = intermediateResult.replace(/%\(AUTHOR\)/g, author);
+  intermediateResult = intermediateResult.replace(/%\(CLASS_NAME\)/g, className);
+  intermediateResult = intermediateResult.replace(/%\(DISPLAY_NAME\)/g, name);
+  intermediateResult = intermediateResult.replace(/%\(ITEM_UUID\)/g, itemUuid);
+  intermediateResult = intermediateResult.replace(/%\(ENTRY_UUID_1\)/g, entryUuid1);
+  intermediateResult = intermediateResult.replace(/%\(ENTRY_UUID_2\)/g, entryUuid2);
+  intermediateResult = intermediateResult.replace(/%\(ENTRY_UUID_3\)/g, entryUuid3);
+  intermediateResult = intermediateResult.replace(/%\(ENTRY_UUID_4\)/g, entryUuid4);
+  var finalString = intermediateResult;
+  
+  return finalString;
+}
+
+dojo.event.connect(window, "onload", startHere);
+
+
+// -------------------------------------------------------------------
+// End of file
+// -------------------------------------------------------------------
---
+++ trunk/source/plugins/create_new_plugin/create_new_plugin_template.js
@@ -0,0 +1,110 @@
+// %(CLASS_NAME).js
+// Written by %(AUTHOR)
+
+// Dojo Package System "provide" and "require" statements
+dojo.provide("orp.plugins.%(CLASS_NAME)");
+dojo.require("orp.view.PluginView");
+
+/**
+ * The %(CLASS_NAME) view displays a set of content items. 
+ *
+ * @scope    public instance constructor
+ * @extends  PluginView
+ * @param    superview    The View that serves as the superview for this view. 
+ * @param    htmlElement    The HTMLElement to display this view in. 
+ * @param    querySpec    The Query Spec item that provides the items for this PluginView to display
+ * @param    layoutItem    An item that can be used to store layout data (like table column order). 
+ */
+orp.plugins.%(CLASS_NAME) = function(superview, htmlElement, querySpec, layoutItem) {
+  orp.view.PluginView.call(this, superview, htmlElement, querySpec, layoutItem, "%(CLASS_NAME)");
+};
+
+dojo.inherits(orp.plugins.%(CLASS_NAME), orp.view.PluginView);  // makes %(CLASS_NAME) be a subclass of PluginView
+
+
+// -------------------------------------------------------------------
+// Register this plugin in the SectionView registry
+// -------------------------------------------------------------------
+orp.plugins.%(CLASS_NAME).UUID = "%(ITEM_UUID)";
+orp.view.SectionView.registerPlugin(orp.plugins.%(CLASS_NAME));
+
+
+// -------------------------------------------------------------------
+// Public class methods
+// -------------------------------------------------------------------
+
+/**
+ * Returns the UUID of the item that represents this class of plugin.
+ *
+ * @scope    public class method
+ * @return   The UUID of the item that represents this class of plugin.
+ */
+orp.plugins.%(CLASS_NAME).getPluginItemUuid = function() {
+  return orp.plugins.%(CLASS_NAME).UUID;
+};
+
+/**
+ * Returns a list of anonymous objects representing Entries that describe the plugin.
+ *
+ * @scope    public class method
+ * @return   A list of anonymous objects representing Entries that describe the plugin.
+ */
+orp.plugins.%(CLASS_NAME).getEntriesForItemRepresentingPluginClass = function(pluginItem, world) {
+  return [
+    { uuid: "%(ENTRY_UUID_1)",
+      item: pluginItem,
+      attribute: world.getAttributeCalledName(),
+      value: "%(DISPLAY_NAME)" },
+    { uuid: "%(ENTRY_UUID_2)",
+      item: pluginItem,
+      attribute: world.getAttributeCalledShortName(),
+      value: "%(CLASS_NAME)" },
+    { uuid: "%(ENTRY_UUID_3)",
+      item: pluginItem,
+      attribute: world.getAttributeCalledClassName(),
+      value: "%(CLASS_NAME)" },
+    { uuid: "%(ENTRY_UUID_4)",
+      item: pluginItem,
+      attribute: world.getAttributeCalledCategory(),
+      inverseAttribute: world.getAttributeCalledItemsInCategory(),
+      value: world.getItemFromUuid(orp.view.SectionView.UUID.CATEGORY_PLUGIN_VIEW) }
+  ];
+};
+
+// -------------------------------------------------------------------
+// Public instance methods
+// -------------------------------------------------------------------
+
+/**
+ * Returns the class of this instance.
+ *
+ * @scope    public instance method
+ * @return   A JavaScript class. 
+ */
+orp.plugins.%(CLASS_NAME).prototype.getClass = function() {
+  return orp.plugins.%(CLASS_NAME);
+};
+
+/**
+ * This method is called whenever the %(CLASS_NAME) plugin is used to
+ * display the results of a query.
+ *
+ * @scope    public instance method
+ */
+orp.plugins.%(CLASS_NAME).prototype.refresh = function() {
+  var listOfContentItems = this.fetchItems();
+  var outlineDiv = this.getHtmlElement();
+
+  // %(AUTHOR):
+  //  This is just example code.
+  //  You should replace the code below with your own code.
+  orp.view.View.removeChildrenOfElement(outlineDiv);
+  var ulElement = orp.view.View.appendNewElement(outlineDiv, "ul");
+  for (var i in listOfContentItems) {
+    var contentItem = listOfContentItems[i];
+    var liText = contentItem.getDisplayName("{no name}") + " ";
+    var liElement = orp.view.View.appendNewElement(ulElement, "li", null, null, liText);
+  }
+};
+
+// End of file
