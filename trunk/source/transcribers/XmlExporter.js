cb2bed77a4c4b9e90eae175a527b68fd07381d03
skinner <skinner@fa7cf185-f9f2-0310-86ed-83ad1049b242>
2005-12-16 21:07:01 +0000

    ooops, forgot to check in XmlExporter.js last time.

---
+++ trunk/source/transcribers/XmlExporter.js
@@ -0,0 +1,54 @@
+/*****************************************************************************
+ XmlExporter.js
+ 
+******************************************************************************
+ Written in 2005 by Brian Douglas Skinner <brian.skinner@gumption.org>
+  
+ Copyright rights relinquished under the Creative Commons  
+ Public Domain Dedication:
+    http://creativecommons.org/licenses/publicdomain/
+  
+ You can copy freely from this file.  This work may be freely reproduced, 
+ distributed, transmitted, used, modified, built upon, or otherwise exploited
+ by anyone for any purpose.
+  
+ This work is provided on an "AS IS" basis, without warranties or conditions 
+ of any kind, either express or implied, including, without limitation, any 
+ warranties or conditions of title, non-infringement, merchantability, or 
+ fitness for a particular purpose. You are solely responsible for determining 
+ the appropriateness of using or distributing the work and assume all risks 
+ associated with use of this work, including but not limited to the risks and 
+ costs of errors, compliance with applicable laws, damage to or loss of data 
+ or equipment, and unavailability or interruption of operations.
+
+ In no event shall the authors or contributors have any liability for any 
+ direct, indirect, incidental, special, exemplary, or consequential damages,
+ however caused and on any theory of liability, whether in contract, strict 
+ liability, or tort (including negligence), arising in any way out of or in 
+ connection with the use or distribution of the work.
+*****************************************************************************/
+
+
+// -------------------------------------------------------------------
+// Provides and Requires
+// -------------------------------------------------------------------
+dojo.provide("orp.transcribers.XmlExporter");
+
+// -------------------------------------------------------------------
+// Constructor
+// -------------------------------------------------------------------
+
+/**
+ * The XmlExporter class knows how to take a set of items and serialize
+ * them to an XML file.
+ *
+ * @scope    public instance constructor
+ */
+orp.transcribers.XmlExporter = function() {
+  dojo.unimplemented("orp.transcribers.XmlExporter");
+};
+
+
+// -------------------------------------------------------------------
+// End of file
+// -------------------------------------------------------------------
